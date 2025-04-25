c1ecd14f85829d1abda891ea788aceda6ca395ca
will reginald <126414140+Willnighter@users.noreply.github.com>
2025-04-25 21:30:13 +0800

    Add files via upload

--- lifepath.py
+++ lifepath.py
@@ -144,7 +144,7 @@ else:
     music_links = {
         "pathfinder": "sr8bE87AlaM",
         "resonant": "3nQNiWdeH2Q",
-        "weaver": "8ZtInClXe1Q",
+        "weaver": "8ci2wLu81eA",
         "dreamer": "E8E4zRzzyVM",
         "architect": "hn3wJ1_1Zsg",
         "challenger": "btm8qQUmPqY"
